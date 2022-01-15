0b12f37439ce125abea7685711c2b506a5eaa042
Rahul Somani <rsomani95@gmail.com>
2022-01-15 18:16:47 +0530

    feature - drawing text over img

--- upyog/image/draw.py
+++ upyog/image/draw.py
@@ -154,3 +154,135 @@ def make_img_grid(
             print(prog)
 
     return grid
+
+
+color = lambda r, g, b: f"rgb({r},{g},{b})"
+
+
+@fastcore.patch
+def draw_text(
+    self: PIL.Image.Image,
+    text: Union[str, List[str]],
+    font_size: float = None,
+    font_color: Union[tuple, int] = (220, 220, 220),
+    font_path: Optional[str] = str(
+        Path(__file__).parent.parent.parent / "assets" / "EuroStyleNormal.ttf"
+    ),
+    location: Literal[
+        "bottom",
+        "bottom-right",
+        "bottom-left",
+        "top",
+        "top-right",
+        "top-left",
+    ] = "top",
+    font_background: bool = False,
+    font_border: bool = True,
+    font_size_div_factor: int = 30,
+    top_pad_factor: float = 0.001,
+):
+    """
+    Draw `text` on top of `img`. This is done _inplace_
+
+    * text: The text that will be "drawn" on the image. Can be a
+              list of strings as well
+    * font_size: Explicitly set font-size. Not recommended.
+                 See `font_size_div_factor` to scale font size proportionately
+    * font_color: An int or tuple of RGB color values
+    * font_path: Path to the font file (tested with `.ttf`)
+    * location: Where to draw the text? Any combination of
+                {bottom,top} + {right,left} where {bottom,top}
+                are mandatory
+    * font_background: Draw a black rectangle behind the text for clarity.
+                       WARNING: This modifies the image size
+    * font_size_div_factor: Set font size to img.width/font_size_div_factor
+                            Font size is smaller for larger values
+    * top_pad_factor: Adjustment for the padding of the text from top. This
+                      may need some fiddling depending on the font being used
+    """
+    img = self
+
+    assert location in [
+        "bottom",
+        "bottom-right",
+        "bottom-left",
+        "top",
+        "top-right",
+        "top-left",
+    ]
+
+    if font_size is None:
+        font_size = int(img.width / font_size_div_factor)
+
+    if font_path:
+        # PIL gives an unintuitive OSError without much useful info
+        if not Path(font_path).exists:
+            raise FileNotFoundError(f"Couldn't find font file @ {font_path}")
+        font = ImageFont.truetype(font_path, size=font_size)
+
+    else:
+        font = ImageFont.load_default()
+        warnings.warn(
+            "Loaded default PIL ImageFont. It's highly recommended you use a custom font as the default font's size cannot be tweaked"
+        )
+
+    fcolor = color(*font_color)
+    # Check for valid locations
+
+    # Convert text to drawable format
+    if isinstance(text, str):
+        text = [text]
+
+    if font_background:
+        if len(text) > 1:
+            warnings.warn(
+                f"Drawing a font background with multiple texts will likely result in a misaligned title. Split your code into multiple calls with a dedicated call for the title"
+            )
+        h = int(font_size * 1.7)
+        i = Image.new("RGB", (img.width, h + img.height))
+
+        paste_coords = (0, 0) if "bottom" in location else (0, h)
+        i.paste(img, paste_coords)
+        img = i
+
+    if "top" in location:
+        text = ["\n".join(text)]
+
+    draw = ImageDraw.Draw(img)
+
+    for i, label in enumerate(list(reversed(text))):
+        if "bottom" in location or "top" in location:
+            w, h = draw.textsize(label, font)
+
+        if "bottom" in location:
+            height = img.height - ((i + 1) * font_size * 1.3)
+
+        elif "top" in location:
+            height = img.height * font_size * top_pad_factor
+
+        if location == "bottom" or location == "top":
+            xy = ((img.width - w) / 2, height)
+        elif "-right" in location:
+            xy = (((img.width - (w + img.width * 0.01))), height)
+        elif "-left" in location:
+            xy = (img.width * 0.01, height)
+        else:
+            y = 1 if i == 0 else i * font_size * 1.5
+            xy = (10, y)
+
+        if font_border:
+            # thin border
+            x, y = xy
+            draw.text((x - 1, y), label, font=font, fill="black")
+            draw.text((x + 1, y), label, font=font, fill="black")
+            draw.text((x, y - 1), label, font=font, fill="black")
+            draw.text((x, y + 1), label, font=font, fill="black")
+
+            # thicker border
+            draw.text((x - 1, y - 1), label, font=font, fill="black")
+            draw.text((x + 1, y - 1), label, font=font, fill="black")
+            draw.text((x - 1, y + 1), label, font=font, fill="black")
+            draw.text((x + 1, y + 1), label, font=font, fill="black")
+
+        draw.text(xy, text=label, fill=fcolor, font=font)
+    return img

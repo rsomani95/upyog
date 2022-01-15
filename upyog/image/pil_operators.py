4acc9deaee87dde5cc3e15a3ca39d868892921cc
Rahul Somani <rsomani95@gmail.com>
2022-01-15 11:51:44 +0530

    fix padding, img grid creator

--- upyog/image/pil_operators.py
+++ upyog/image/pil_operators.py
@@ -1,13 +1,20 @@
 from upyog.imports import *
+from upyog.image.io import *
 
 
 @fastcore.patch
 def __or__(self: Image.Image, other: Image.Image):
     "Horizontally stack two PIL Images"
     assert isinstance(other, Image.Image)
-    widths, heights = zip(*(i.size for i in [self, other]))
+    this_W, this_H = self.size
+    that_W, that_H = other.size
 
-    new_img = Image.new("RGB", (sum(widths), max(heights)))
+    if not this_H == that_H:
+        other = other.resize_pad((that_W, this_H))
+
+    widths, heights = zip(*(i.size for i in [self, other]))
+    new_img = Image.new("RGB", (sum(widths), max(heights)), 255)
+
     x_offset = 0
     for img in [self, other]:
         new_img.paste(img, (x_offset, 0))
@@ -19,9 +26,15 @@ def __or__(self: Image.Image, other: Image.Image):
 def __floordiv__(self: Image.Image, other: Image.Image):
     "Vertically stack two PIL Images"
     assert isinstance(other, Image.Image)
-    widths, heights = zip(*(i.size for i in [self, other]))
+    this_W, this_H = self.size
+    that_W, that_H = other.size
 
-    new_img = Image.new("RGB", (max(widths), sum(heights)))
+    if not this_W == that_W:
+        other = other.resize_pad((this_W, that_H))
+
+    widths, heights = zip(*(i.size for i in [self, other]))
+    new_img = Image.new("RGB", (max(widths), sum(heights)), 255)
+
     y_offset = 0
     for img in [self, other]:
         new_img.paste(img, (0, y_offset))
@@ -29,9 +42,84 @@ def __floordiv__(self: Image.Image, other: Image.Image):
     return new_img
 
 
-def img_join_horizontal(imgs: List[Image.Image]) -> Image.Image:
+ImageCollection = Collection[Image.Image]
+
+
+def img_join_horizontal(imgs: ImageCollection) -> Image.Image:
     return reduce(operator.__or__, imgs)
 
 
-def img_join_vertical(imgs: List[Image.Image]) -> Image.Image:
+def img_join_vertical(imgs: ImageCollection) -> Image.Image:
     return reduce(operator.__floordiv__, imgs)
+
+
+def img_grid(
+    imgs: ImageCollection, ncol=3, size_WH: Optional[tuple] = (640, 384), pad=True
+):
+    start = time.time()
+
+    # fmt: off
+    if size_WH:
+        if pad: imgs = [img.resize_pad(size_WH) for img in imgs]
+        else:   imgs = [img.resize(size_WH) for img in imgs]
+    else:
+        pass
+    # fmt: on
+
+    rows, row = [], []
+    for i, img in enumerate(imgs, start=1):
+        row += [img]
+        if i % ncol == 0:
+            rows += [row]
+            row = []
+    rows += [row]
+
+    # return rows
+    print("Creating Grid")
+    rows = [img_join_horizontal(row) for row in rows]
+
+    end = time.time()
+
+    return img_join_vertical(rows)
+
+
+def img_grid(
+    imgs: ImageCollection, ncol=3, size_WH: Optional[tuple] = (640, 384), pad=True
+):
+    from rich.progress import Progress
+
+    with Progress() as prog:
+        # fmt: off
+        if size_WH:
+            if pad: imgs = [img.resize_pad(size_WH) for img in imgs]
+            else:   imgs = [img.resize(size_WH) for img in imgs]
+        else:
+            pass
+        # fmt: on
+
+        # Create rows with individual images
+        rows, row = [], []
+        for i, img in enumerate(imgs, start=1):
+            row += [img]
+            if i % ncol == 0:
+                rows += [row]
+                row = []
+        rows += [row]
+
+        n_tasks = len(rows) + 1
+        task_concat = prog.add_task("Concatenating Rows...", total=n_tasks)
+        # print(task_concat.total)
+        grid = []
+
+        while not prog.finished:
+            for row in rows:
+                grid += [img_join_horizontal(row)]
+                prog.update(task_concat, advance=1)
+                time.sleep(1)
+
+            grid = img_join_vertical(grid)
+            prog.update(task_concat, advance=1)
+
+            print(prog)
+
+    return grid

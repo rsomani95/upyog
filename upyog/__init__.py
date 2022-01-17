c2230f5849c0a309d6621cc52f92efc6e7531eb4
Rahul Somani <rsomani95@gmail.com>
2022-01-17 08:31:35 +0530

    use rich traceback only outside nb

--- upyog/__init__.py
+++ upyog/__init__.py
@@ -1,13 +1,30 @@
 from upyog.imports import *
 
 
-install()
 logger.opt(colors=True)
 tqdm.pandas()
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
 pil_version = PIL.__version__
+
+
+# https://stackoverflow.com/questions/15411967/how-can-i-check-if-code-is-executed-in-the-ipython-notebook
+def in_notebook():
+    try:
+        from IPython import get_ipython
+
+        if "IPKernelApp" not in get_ipython().config:  # pragma: no cover
+            return False
+    except ImportError:
+        return False
+    except AttributeError:
+        return False
+    return True
+
+
+if not in_notebook():
+    install()
 
 if not "post" in pil_version:
     msg = f"Pillow-SIMD not installed. Using PIL {pil_version} instead"

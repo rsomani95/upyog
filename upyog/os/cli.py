dc73775d2aa9b8ae596a01d3cef32e185fdc37e4
Rahul Somani <rsomani95@gmail.com>
2022-01-18 18:33:15 +0530

    duplicate removal utility

--- upyog/os/cli.py
+++ upyog/os/cli.py
@@ -11,7 +11,7 @@ def move_files(
     move: P("Move files (desctructively)", action="store_true") = False,
 ):
     """
-    File Mover
+            --------- FILE MOVER ---------
 
     Move files from an arbitrary number of input folders to a _single_ output folder.
 
@@ -27,5 +27,66 @@ def move_files(
 
     mover = shutil.move if move else shutil.copy
 
-    for file in track(files):
+    for file in track(files, "Moving Files..."):
         mover(file, out_dir)
+
+
+@call_parse
+def cleanup_duplicates(
+    i: P("Single input folder that is to be cleaned up", str) = None,
+    t: P(
+        "An arbitrary number of target folders that contain duplicate files that are to be removed from the input folder `--i`",
+        str,
+        nargs="+",
+    ) = None,
+):
+    """
+            --------- DUPLICATE REMOVER ---------
+
+    This program scans the _names_ of the files in the input and target folders
+    recursively, and removes all the files from the `input` folder that exist in
+    `target` folders
+
+    If input folder has the following files:
+    ├── input-folder
+    │   ├── file1.jpg
+    │   ├── file2.jpg
+    │   └── file3.jpg
+
+    And the target folder(s) have the following files:
+    ├── target-folder-1
+    │   ├── file1.jpg
+    │   ├── other-random-file-1.jpg
+
+    ├── target-folder-2
+    │   ├── file2.jpg
+    │   ├── other-random-file-2.jpg
+
+    Using this program would remove `file1.jpg` and `file2.jpg` from the input folders
+
+    Usage:
+    ------
+
+    remove-duplicates-from-folder
+        --i input-folder
+        --t target-folder-1   target-folder-2   ...
+    """
+    cleanup_path = Path(i)
+    target_paths = [Path(p) for p in t]
+
+    target_fnames = flatten([get_image_files(path) for path in target_paths])
+    target_fnames = [f.stem for f in target_fnames]
+
+    cleanup_files = get_image_files(cleanup_path)
+    cleanup_fnames = [f.stem for f in cleanup_files]
+
+    fnames_to_delete = set.intersection(set(cleanup_fnames), set(target_fnames))
+
+    counter = 0
+    for f in track(cleanup_files, "Scanning Files..."):
+        if f.stem in fnames_to_delete:
+            print(f"Deleting {f.name}")
+            counter += 1
+            f.unlink()
+
+    print(f"Deleted {counter} files")

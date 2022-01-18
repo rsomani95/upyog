e1145ac6e565dca8fae3dff34fd069f18bbd2103
Rahul Somani <rsomani95@gmail.com>
2022-01-18 18:35:05 +0530

    move stuff around

--- upyog/image/composition.py
+++ upyog/image/composition.py
@@ -5,3 +5,59 @@ def draw_rule_of_thirds(img: Image.Image, thickness=5, opacity=0.4):
     img = img.draw_vertical_bars([1 / 3, 2 / 3], thickness=thickness, opacity=opacity)
     img = img.draw_horizontal_bars([1 / 3, 2 / 3], thickness=thickness, opacity=opacity)
     return img
+
+
+ImageCollection = Collection[Image.Image]
+
+
+def img_join_horizontal(imgs: ImageCollection) -> Image.Image:
+    return reduce(operator.__or__, imgs)
+
+
+def img_join_vertical(imgs: ImageCollection) -> Image.Image:
+    return reduce(operator.__floordiv__, imgs)
+
+
+def make_img_grid(
+    imgs: ImageCollection, ncol=3, size_WH: Optional[tuple] = (640, 384), pad=True
+):
+    # fmt: off
+    if size_WH:
+        if pad: imgs = [img.resize_pad(size_WH) for img in imgs]
+        else:   imgs = [img.resize(size_WH) for img in imgs]
+    else:
+        pass
+    # fmt: on
+
+    if ncol == 1:
+        return img_join_vertical(imgs)
+
+    from rich.progress import Progress
+
+    with Progress() as prog:
+        # Create rows with individual images
+        rows, row = [], []
+        for i, img in enumerate(imgs, start=1):
+            row += [img]
+            if i % ncol == 0:
+                rows += [row]
+                row = []
+        if not row == []:
+            rows += [row]
+
+        n_tasks = len(rows) + 1
+        task_concat = prog.add_task("Concatenating Rows...", total=n_tasks)
+        # print(task_concat.total)
+        grid = []
+
+        while not prog.finished:
+            for row in rows:
+                grid += [img_join_horizontal(row)]
+                prog.update(task_concat, advance=1)
+
+            grid = img_join_vertical(grid)
+            prog.update(task_concat, advance=1)
+
+            print(prog)
+
+    return grid
--- upyog/image/draw.py
+++ upyog/image/draw.py
@@ -150,62 +150,6 @@ def draw_horizontal_bars(img, height_percentages, fill, thickness, opacity):
     return draw_rectangles(img, xyxys, opacity=opacity, fill=fill)
 
 
-ImageCollection = Collection[Image.Image]
-
-
-def img_join_horizontal(imgs: ImageCollection) -> Image.Image:
-    return reduce(operator.__or__, imgs)
-
-
-def img_join_vertical(imgs: ImageCollection) -> Image.Image:
-    return reduce(operator.__floordiv__, imgs)
-
-
-def make_img_grid(
-    imgs: ImageCollection, ncol=3, size_WH: Optional[tuple] = (640, 384), pad=True
-):
-    # fmt: off
-    if size_WH:
-        if pad: imgs = [img.resize_pad(size_WH) for img in imgs]
-        else:   imgs = [img.resize(size_WH) for img in imgs]
-    else:
-        pass
-    # fmt: on
-
-    if ncol == 1:
-        return img_join_vertical(imgs)
-
-    from rich.progress import Progress
-
-    with Progress() as prog:
-        # Create rows with individual images
-        rows, row = [], []
-        for i, img in enumerate(imgs, start=1):
-            row += [img]
-            if i % ncol == 0:
-                rows += [row]
-                row = []
-        if not row == []:
-            rows += [row]
-
-        n_tasks = len(rows) + 1
-        task_concat = prog.add_task("Concatenating Rows...", total=n_tasks)
-        # print(task_concat.total)
-        grid = []
-
-        while not prog.finished:
-            for row in rows:
-                grid += [img_join_horizontal(row)]
-                prog.update(task_concat, advance=1)
-
-            grid = img_join_vertical(grid)
-            prog.update(task_concat, advance=1)
-
-            print(prog)
-
-    return grid
-
-
 color = lambda r, g, b: f"rgb({r},{g},{b})"
 
 

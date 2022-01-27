31ee4137538e263b2b8f62077b7c7727a4b0d248
Rahul Somani <rsomani95@gmail.com>
2022-01-27 10:46:23 +0530

    make more generic - works with arrays, series, etc

--- upyog/utils/utils.py
+++ upyog/utils/utils.py
@@ -7,7 +7,7 @@ __all__ = ["flatten", "uniqueify"]
 def flatten(x: Any) -> List[Any]:
     flattened_list = []
     for item in x:
-        if isinstance(item, (tuple, list)):
+        if hasattr(item, "__iter__") and not isinstance(item, (str, bytes)):
             [flattened_list.append(i) for i in item]
         else:
             flattened_list.append(item)

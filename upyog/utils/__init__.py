95c3c1ae6fc18739ad2419c529989703e8a480c9
Rahul Somani <rsomani95@gmail.com>
2022-08-08 16:48:28 +0530

    add zip utils

--- upyog/utils/__init__.py
+++ upyog/utils/__init__.py
@@ -1,3 +1,4 @@
 from .utils import *
 from .boxes import *
 from .download import *
+from .zip import *
---
+++ upyog/utils/zip.py
@@ -0,0 +1,25 @@
+from upyog.imports import *
+
+def zip_folder(path: PathLike):
+    path = Path(path)
+    assert path.is_dir()
+    assert path.exists()
+
+    return shutil.make_archive(path, "zip", path)
+
+def compose_files_into_zip_archive(
+    files: List[PathLike],
+    out_dir: PathLike,
+    zip_name: str = "Archive",
+):
+    out_dir = Path(out_dir)
+    out_dir.mkdir(exist_ok=True)
+    zip_name = zip_name + ".zip"
+
+    for file in files:
+        shutil.copy(file, str(out_dir / file.name))
+
+    zipfile = zip_folder(out_dir)
+    shutil.rmtree(out_dir)
+
+    return zipfile

c1f33fe2a87ce4fec868242bdafaf5873ded8012
Rahul Somani <rsomani95@gmail.com>
2023-06-25 00:18:21 -0400

    wip commit - metrics visualiser

--- upyog/ml/visualise.py
+++ upyog/ml/visualise.py
@@ -1,9 +1,49 @@
 from upyog.ml.imports import *
+from upyog.ml.utils import to_np, to_torch
 from upyog.image import fig_to_pil
 
 
 __all__ = ["visualise_conf_matrix"]
 
+
+class MetricsVisualiser:
+    def __init__(
+        self,
+        ground_truths: Union[Collection[float], Collection[str]],
+        predictions: Union[Collection[float], Collection[str]],
+    ):
+        self.ground_truths = to_np(ground_truths, dtype=None)
+        self.predictions = to_np(predictions, dtype=None)
+
+    @classmethod
+    def from_df(
+        cls,
+        df: pd.DataFrame, ground_truths_colname: str, predictions_colname: str, 
+    ):
+        return cls(
+            ground_truths = df[ground_truths_colname].values.tolist(),
+            predictions = df[predictions_colname].values.tolist(),
+        )
+
+    def visualise_confusion_matrix():
+        ...
+
+    def visualise_metrics():
+        ...
+
+    def get_classification_report():
+        ...
+
+
+def get_confusion_matrix(
+    ground_truths: Union[List[float], List[str]],
+    predictions: Union[List[float], List[str]],
+):
+    from sklearn.metrics import confusion_matrix
+
+    if not len(ground_truths) == len(predictions):
+        raise RuntimeError(f"Mismatch between `len(ground_truths)` and `len(predictions)`")
+    return confusion_matrix(ground_truths, predictions)
 
 def visualise_conf_matrix(
     conf_matrix: np.ndarray,

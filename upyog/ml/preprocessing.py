7acb1a8123eed48d1439bf8622fb78225dd66972
Rahul Somani <rsomani95@gmail.com>
2023-07-27 11:56:45 -0400

    add pil interpolation map

--- upyog/ml/preprocessing.py
+++ upyog/ml/preprocessing.py
@@ -8,13 +8,22 @@ IMAGENET_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_STDEV = (0.229, 0.224, 0.225)
 
 
+_pil_interpolation_map = {
+     "bicubic" : Image.Resampling.BICUBIC,
+    "bilinear" : Image.Resampling.BILINEAR,
+     "lanczos" : Image.Resampling.LANCZOS,
+     "nearest" : Image.Resampling.NEAREST,
+         "box" : Image.Resampling.BOX,
+     "hamming" : Image.Resampling.HAMMING,
+}
+
 @dataclass
 class PreprocessingParams:
     input_height: int
     input_width: int
     normalisation_mean: Optional[List[float]]
     normalisation_stdev: Optional[List[float]]
-    interpolation: Literal["bilinear", "bicubic"]
+    interpolation: str
     output_format: Literal["BGR", "RGB"]
     resize_method: Literal["squish", "pad"]
     pad_fill: Optional[Tuple[int, int, int]] = None
@@ -25,14 +34,18 @@ class PreprocessingParams:
             if x > 1.0:
                 raise RuntimeError(f"Normalisation stats should be between 0-1. Got {stats}")
 
+    @property
+    def pil_interpolation_method(self) -> Image.Resampling:
+        return _pil_interpolation_map[self.interpolation]
 
     def __post_init__(self):
         if self.normalisation_mean is not None:  self._check_stats_in_0_to_1_range(self.normalisation_mean)
         if self.normalisation_stdev is not None: self._check_stats_in_0_to_1_range(self.normalisation_stdev)
-        if not self.interpolation in ("bilinear", "bicubic"): raise ValueError
-        if not self.resize_method in ("squish", "pad"):       raise ValueError
         if not self.output_format in ("BGR", "RGB"):          raise ValueError
-
+        if not self.interpolation in _pil_interpolation_map.keys():
+            raise ValueError(
+                f"resize method '{self.interpolation}' is invalid. Valid choices are: {list(_pil_interpolation_map.keys())}"
+            )
 
     def create_coreml_input(self, input_name: str = "Image") -> List['ct.ImageType']:
         from upyog.ml.coreml_utils import get_coreml_input
